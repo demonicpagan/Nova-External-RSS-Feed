79c3c5101eda7774ac19f522916b7f8e5c79aeb1
Dustin <demonicpagan@gmail.com>
2012-02-10 19:34:24 -0600

    Updated text to represent the MOD. Had text in there for another one of my MODs I've written.
    Signed-off-by: Dustin <demonicpagan@gmail.com>

--- application/config/external_rss.php
+++ application/config/external_rss.php
@@ -4,13 +4,13 @@
 | External RSS Configuration Options
 |--------------------------------------------------------------------------
 |
-| This holds all the needed configuration options for you to place a Mibbit
-| chat page on your Nova installation.
+| This holds all the needed configuration options for you to bring 
+| external RSS feeds to your Nova installation.
 |
 */
-$config['rss_url'] = "http://<yoursite>/feed";
+$config['rss_url'] = "http://groups.google.com/group/anodyne-productions-nova-3rd-party-developement/feed/rss_v2_0_topics.xml?num=50";
 $config['rss_limit'] = "15";
-$config['rss_site_url'] = "http://<yoursite>";
+$config['rss_site_url'] = "http://groups.google.com/group/anodyne-productions-nova-3rd-party-developement/topics";
 $config['rss_cache_enable'] = TRUE;
 $config['rss_cahce_location'] = APPPATH."cache/rss";
 $config['rss_cache_time'] = "3600"; // 1 Hour
